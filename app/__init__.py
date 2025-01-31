7c17fe152cd9a558fe48e3ec0c6b18cc81a5a285
Sh3v4n10 <daviid.luuna@outlook.es>
2025-01-31 20:51:13 +0100

    1.1.0.1

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
 
 # Extensions
 db = SQLAlchemy()
+login_manager = LoginManager()
 mail = Mail()
 csrf = CSRFProtect()
 
@@ -14,9 +16,12 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
 
     # Initialize extensions
+    # db.init_app(app)
+    # mail.init_app(app)
+    # csrf.init_app(app)
     db.init_app(app)
-    mail.init_app(app)
-    csrf.init_app(app)
+    login_manager.init_app(app)  # Asegura que LoginManager está inicializado
+    login_manager.login_view = "main.login"  # Página de inicio de sesión
 
     # Register blueprints or routes
     from app.routes import main_bp
